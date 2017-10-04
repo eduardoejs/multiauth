aaf76d8586f7f46c0832d6e4a541eb9b135b9198
Eduardo Silva <eduardo.ejs@hotmail.com>
2017-10-04 18:46:16 +0000

    Primeiros ajustes para multiple authentications. Definidos os guards e providers no config/auth.php. Foi criado o Model e Controller de Admin. Ajustado o Dashboard de Users e Admin - Blades Files

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //permite acesso somente a pessoas autenticadas e
+        //permite autenticar somente as pessoas que possuem o guard admin
+        //se falhar será redirecionado para o login default - guard default
+        $this->middleware('auth:admin');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('user.dashboard');
+Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
